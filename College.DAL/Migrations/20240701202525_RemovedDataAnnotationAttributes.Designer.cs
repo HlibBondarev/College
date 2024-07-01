bbc96a7727262072b012f255e32c44933c756640
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-07-01 23:31:59 +0300

    Add Migration - RemovedDataAnnotationAttributes

--- College.DAL/Migrations/20240701202525_RemovedDataAnnotationAttributes.Designer.cs
+++ College.DAL/Migrations/20240701202525_RemovedDataAnnotationAttributes.Designer.cs
@@ -4,6 +4,7 @@ using College.DAL.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace College.DAL.Migrations
 {
     [DbContext(typeof(CollegeDbContext))]
-    partial class CollegeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240701202525_RemovedDataAnnotationAttributes")]
+    partial class RemovedDataAnnotationAttributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
