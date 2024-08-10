d436d4d72c6ac7711f51d5a360a2122784f8dcc8
HlibBondarev <Hlib.Bondarev@gmail.com>
2024-08-10 14:05:05 +0300

    Refactoring SeedRoles and SeedAdmin methods in ServiceProviderExtensions class

--- College.WebApi/Extensions/ServiceProviderExtensions.cs
+++ College.WebApi/Extensions/ServiceProviderExtensions.cs
@@ -8,39 +8,72 @@ public static class ServiceProviderExtensions
     public static async Task SeedRoles(this IServiceProvider app, WebApplicationBuilder builder)
     {
         var roleManager = app.GetRequiredService<RoleManager<IdentityRole>>();
+        var loggerFactory = app.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<Program>();
+
         var roles = builder.Configuration.GetSection("Authentication").GetSection("Roles").Get<IEnumerable<string>>();
 
-        foreach (var role in roles!)
+        try
         {
-            if (!await roleManager.RoleExistsAsync(role))
+            if (roles is null || !roles.Any())
             {
-                await roleManager.CreateAsync(new IdentityRole(role));
+                string errorMsg = string.Format(
+                "There are no roles in the Authentication.Roles section of the appsettings.json file.");
+                throw new NullReferenceException(errorMsg);
             }
+
+            foreach (var role in roles!)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, string.Format("An error occurred seeding the DB. {0}", ex.Message));
         }
     }
 
     public static async Task SeedAdmin(this IServiceProvider app, WebApplicationBuilder builder)
     {
         var userManager = app.GetRequiredService<UserManager<ApplicationUser>>();
+        var loggerFactory = app.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<Program>();
 
-        string userName = builder.Configuration.GetSection("Authentication").GetSection("Admin").GetValue<string>("Email")!;
-        string password = builder.Configuration.GetSection("Authentication").GetSection("Admin").GetValue<string>("Password")!;
+        string? userName = builder.Configuration.GetSection("Authentication").GetSection("Admin").GetValue<string>("Email");
 
-        var existingUser = await userManager.FindByNameAsync(userName!);
-
-        if (existingUser == null)
+        try
         {
-            var user = new ApplicationUser
+            if (userName is null)
             {
-                UserName = userName,
-                Email = userName
-            };
-
-            var result = await userManager.CreateAsync(user, password!);
-            if (result.Succeeded)
-            {
-                await userManager.AddToRoleAsync(user, builder.Configuration.GetSection("Authentication").GetSection("Roles").Get<List<string>>()![0]);
+                string errorMsg = string.Format(
+                "The Email parameter in the Authentication.Admin section of the appsettings.json (or secret.json) file is not filled in.");
+                throw new NullReferenceException(errorMsg);
             }
+
+            var existingUser = await userManager.FindByNameAsync(userName);
+
+            if (existingUser is null)
+            {
+                string password = builder.Configuration.GetSection("Authentication").GetSection("Admin").GetValue<string>("Password") ?? string.Empty;
+                var user = new ApplicationUser
+                {
+                    UserName = userName,
+                    Email = userName
+                };
+
+                var result = await userManager.CreateAsync(user, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, builder.Configuration.GetSection("Authentication").GetSection("Roles").Get<List<string>>()![0]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, string.Format("An error occurred seeding the DB. {0}", ex.Message));
         }
     }
 }
